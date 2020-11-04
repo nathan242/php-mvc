54f27bf4c0cd82eb8408116f7aa86d3abef6df67
Nathan Patrizi <nathan.patrizi@gmail.com>
2020-11-04 15:16:26 +0000

    Add SQLite3 DB support

--- config/db.php
+++ config/db.php
@@ -1,6 +1,8 @@
 <?php
     use db\mdb;
+    use db\sqlite3db;
     use db\mysql_builder;
+    use db\sqlite3_builder;
 
     return [
         'driver' => mdb::class,
@@ -10,3 +12,9 @@
         'password' => 'P4p_Mvc!!2019',
         'db' => 'php_mvc'
     ];
+
+//    return [
+//        'driver' => sqlite3db::class,
+//        'sql_builder' => sqlite3_builder::class,
+//        'db_filename' => '/srv/sqlite3/database.sqlite3'
+//    ];
