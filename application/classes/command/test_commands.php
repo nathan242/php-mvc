2e3c7eef89444fece8dc87e63ebb7523d81465f9
Nathan Patrizi <nathan.patrizi@gmail.com>
2022-08-10 20:51:39 +0100

    Add CLI method to dump test table data

--- application/classes/command/test_commands.php
+++ application/classes/command/test_commands.php
@@ -2,13 +2,24 @@
     namespace application\command;
 
     use framework\command\base_command;
+    use application\model\test;
 
     class test_commands extends base_command {
+        protected $test_model;
+
+        public function __construct(test $test) {
+            $this->test_model = $test;
+        }
+
         public function dump_config($args = []) {
             if (isset($args[1])) {
                 echo print_r($this->config->get($args[1]), 1)."\n";
             }
 
             return 0;
+        }
+
+        public function show_test_records() {
+            echo print_r($this->test_model->all()->to_array(), true)."\n";
         }
     }
