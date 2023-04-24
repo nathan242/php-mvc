6220c3dfd731972dda9c6958eb591bb94b002536
Nathan Patrizi <nathan.patrizi@gmail.com>
2023-04-24 21:25:17 +0100

    Update README.md

--- Framework/template/config/db.php
+++ Framework/template/config/db.php
@@ -9,13 +9,13 @@ if (!isset($local)) {
 
 $localConfig = $local['db'] ?? [];
 
-    if ($localConfig === []) {
-        return [
-            'driver' => Sqlite3db::class,
-            'sql_builder' => Sqlite3Builder::class,
-            'db_filename' => "{$local['root_path']}/database.sqlite3"
-        ];
-    }
+if ($localConfig === []) {
+    return [
+        'driver' => Sqlite3db::class,
+        'sql_builder' => Sqlite3Builder::class,
+        'db_filename' => "{$local['root_path']}/database.sqlite3"
+    ];
+}
 
-    return $localConfig;
+return $localConfig;
 
