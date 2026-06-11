c83370e77fdf936bdb7c8b5607cbea8468a0304a
Nathan Patrizi <nathan.patrizi@gmail.com>
2026-06-11 22:15:07 +0100

    Update framework to v0.2.6 and fix for PHP 8.5

--- Soap/classes/Soap/Factory/SoapServerFactory.php
+++ Soap/classes/Soap/Factory/SoapServerFactory.php
@@ -5,7 +5,7 @@ use SoapServer;
 
 class SoapServerFactory
 {
-    public function create(string $wsdl = null, array $options = []): SoapServer
+    public function create(?string $wsdl = null, array $options = []): SoapServer
     {
         return new SoapServer($wsdl, $options);
     }
